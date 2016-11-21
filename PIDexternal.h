4f520bf76e122f5b789e42540a8e11d286d52342
Albert Mokrejs <albertmokrejs@gmail.com>
2016-11-20 20:29:51 -0500

    Create PIDexternal.h

---
+++ PIDexternal.h
@@ -0,0 +1,30 @@
+#ifndef PIDexternal_h
+#define PIDexternal_h
+
+#include "Arduino.h"
+
+class PIDexternal
+{
+  public:
+    PIDexternal(float P, float I, float D, float TargetValue, float StartingValue);
+    void setOffset(float Offset);
+    void setP(float P);
+    void setI(float I);
+    void setD(float D);
+    void setTarget(float TargetValue);
+    void step(float CurrentPosition, float Time);
+    float getCorrection();
+    float StepGetC(float CurrentPosition);
+  private:
+    float P;
+    float I;
+    float D;
+    float TVal;
+    float CVal;
+    float LastVal;
+    float Dif;
+    float Offset;
+    boolean includeI;
+};
+
+#endif
