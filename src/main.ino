2a4fcdb87e86b706b85797ae6fc0859a06872558
BrushlessMiku <fangzhou.yu@stonybrook.edu>
2016-12-01 12:46:39 -0500

    Revert "Tune PID loop."
    This reverts commit 6cc450f32914b33ffac12e2399f8ceab34af34b7.

--- src/main.ino
+++ src/main.ino
@@ -22,7 +22,6 @@ void setup(){
   stopAndReset();
   testTimer = millis();
   nTimer = millis();
-  n3Timer = millis();
   //cfg.backToSpot = 0;
   //Serial.println("Hi1");
 
@@ -33,11 +32,11 @@ void loop(){
   //Serial.println("Kalman Pitch: " + (String)pitch);
   uint32_t timer = micros();
   //drive_motor(2200, 2200);
-  if(millis()-testTimer>=1000){
+  if(millis()-testTimer>=500){
     Serial.println("Kalman Value" + (String)pitch);
     testTimer = millis();
   }
-  //drive_motor(1900,1900);
+  //drive_motor(max_pulsewidth,max_pulsewidth);
   updatePID(cfg.targetAngle, 0, 0,(float)(timer-PIDTimer)/1000000.0f);
   //updateEncoder();
   //Serial.println("Motor Speeds" + (String)(1500+i));
@@ -81,12 +80,12 @@ void setValues(){
   cfg.backToSpot = 0;
   //cfg.bindSpektrum = false;
   cfg.controlAngleLimit = 45;
-  cfg.P = 62.00f;
-  cfg.I = 0.15f;
-  cfg.D = 25.0f;
+  cfg.P = 30.0f;
+  cfg.I = 0.00f;
+  cfg.D = 0.0f;
   cfg.leftMotorScaler = 1.0f;
   cfg.rightMotorScaler = 0.97f;
-  cfg.targetAngle = 271.30f;
+  cfg.targetAngle = 272.0f;
   cfg.turningLimit = 2;
-//P:20I:0.005D:8.0
+
 }
--- src/motors.ino
+++ src/motors.ino
@@ -16,13 +16,7 @@ void motor_setup(){
 }
 
 void drive_motor(uint16_t leftMotor, uint16_t rightMotor){
-  uint32_t n2Timer = millis();
 
-  if(n2Timer - n3Timer >= 1000){
-    Serial.println("Left motor: "+(String)leftMotor);
-    Serial.println("right motor: "+(String)rightMotor);
-    n3Timer = n2Timer;
-  }
   if(leftMotor<1500){
     left.writeMicroseconds(leftMotor-75);
   }else if(leftMotor>1500){
@@ -44,35 +38,28 @@ void drive_motor(uint16_t leftMotor, uint16_t rightMotor){
   //Serial.println("Wrote left" + (String)leftMotor + " Wrote right " + (String)rightMotor);
 }
 void updatePID(float restAngle, float offset, float turning, float dt){
-  if(!(abs(pitch - restAngle)>30)){
-    float error = restAngle - pitch;
-    float pTerm = cfg.P * error;
-    integratedError += error*dt;
-    integratedError = constrain(integratedError, -10.0f, 10.0f);
-    float iTerm = cfg.I * integratedError;
-    float dTerm = cfg.D * ((error-lastError) / dt);
-    lastError = error;
-    float PIDValue = pTerm + iTerm + dTerm;
+  float error = restAngle - pitch;
+  float pTerm = cfg.P * error;
+  integratedError += error*dt;
+  integratedError = constrain(integratedError, -100.0f, 100.0f);
+  float iTerm = cfg.I * integratedError;
+  float dTerm = cfg.D * (error-lastError) / dt;
+  lastError = error;
+  float PIDValue = pTerm + iTerm + dTerm;
 
-    float PIDLeft = PIDValue; //+ turning;
-    float PIDRight = PIDValue;// - turning;
-    uint32_t n2Timer = millis();
-
-    if(n2Timer - nTimer >= 1000){
-      Serial.println("Left PID: "+(String)PIDLeft);
-      Serial.println("RightPID: "+(String)PIDRight);
-      nTimer = n2Timer;
-    }
-
-    PIDLeft = constrain(map(PIDLeft, -300, 300, min_pulsewidth, max_pulsewidth), min_pulsewidth, max_pulsewidth);
-    PIDRight = constrain(map(PIDRight, -300, 300, min_pulsewidth, max_pulsewidth), min_pulsewidth, max_pulsewidth);
-
-    drive_motor(PIDLeft, PIDRight);
-  }else if((pitch-restAngle)<0){
-    drive_motor(max_pulsewidth,max_pulsewidth);
-  }else{
-    drive_motor(min_pulsewidth,min_pulsewidth);
+  float PIDLeft = PIDValue; //+ turning;
+  float PIDRight = PIDValue;// - turning;
+  uint32_t n2Timer = millis();
+  if(n2Timer - nTimer >= 100){
+    Serial.println("Left PID: "+(String)PIDLeft);
+    Serial.println("RightPID: "+(String)PIDRight);
+    nTimer = n2Timer;
   }
+
+  PIDLeft = constrain(map(PIDLeft, -300, 300, min_pulsewidth, max_pulsewidth), min_pulsewidth, max_pulsewidth);
+  PIDRight = constrain(map(PIDRight, -300, 300, min_pulsewidth, max_pulsewidth), min_pulsewidth, max_pulsewidth);
+
+  drive_motor(PIDLeft, PIDRight);
 }
 //old Update PID, kept here for reference
 void updatePID2(float restAngle, float offset, float turning, float dt){
